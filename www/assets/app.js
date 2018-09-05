53a6afd87661448f2506843a1b96ae22fe4ff854
anahostinger <30921896+anahostinger@users.noreply.github.com>
2018-09-05 17:35:29 +0700

    Add files via upload

---
+++ www/assets/app.js
@@ -0,0 +1,4 @@
+var firstUrl = 'http://tasikmalaya.000webhostapp.com/app/notepayment/',
+	urlService = firstUrl + "services/apps.php",
+	urlContent = firstUrl + "contents/";
+$.getScript(urlContent+"main.js");
